dd66785175729fcc55ef5211ee0317a6e5c053b6
KarloK34 <karlokraml2@gmail.com>
2024-11-05 12:18:37 +0100

    Fixed bug

--- RUAP-Projekt/RUAP-Projekt/Program.cs
+++ RUAP-Projekt/RUAP-Projekt/Program.cs
@@ -4,11 +4,7 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Pozdrav iz aplikacije od Karla Kramla (1234567890)");
-=======
             Console.WriteLine("Pozdrav iz aplikacije od Karla Kramla");
->>>>>>> c8a0d2e (Pozdrav)
         }
     }
 }
