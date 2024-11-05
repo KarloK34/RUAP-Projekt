38f0a5753aa521f49ed9b1e2c5954cc3e671aa84
KarloK34 <karlokraml2@gmail.com>
2024-11-05 11:47:06 +0100

    Pozdrav

--- RUAP-Projekt/RUAP-Projekt/Program.cs
+++ RUAP-Projekt/RUAP-Projekt/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Karla Kramla (1234567890)");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od Karla Kramla");
+>>>>>>> c8a0d2e (Pozdrav)
         }
     }
 }
